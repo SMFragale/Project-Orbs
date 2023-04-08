2d8b63c677b4a5b27728a054f44c02df6cb6b0ce
Santiago Barbosa Nieto <santibar15@hotmail.com>
2023-04-08 00:42:33 -0500

    Added button to script to manually update the spawns

--- Assets/Scripts/Generation/PathSpawner.cs
+++ Assets/Scripts/Generation/PathSpawner.cs
@@ -1,39 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+
 
 namespace PathCreation
 {
+    [CustomEditor(typeof(PathSpawner))]
+    public class MyScriptEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            PathSpawner myScript = (PathSpawner)target;
+
+            if (GUILayout.Button("Update"))
+            {
+                if (!Application.isPlaying)
+                    return;
+                myScript.UpdateSpawns();
+            }
+        }
+    }
+
     public class PathSpawner : MonoBehaviour
     {
         public PathCreator pathCreator;
         public GameObject toSpawnPrefab;
-        public float interval = 1;
-        float totalDist;
+        private float _totalDist;
+        
+        [SerializeField, Range(0.1f,10.0f)]
+        private float _interval;
+        
 
-        void Start() {
-            Debug.Log("START");
-            if(pathCreator != null)
-                OnPathChanged();
+        public float getInterval()
+        {
+            return _interval;
         }
 
-        void Awake()
+        public void setInterval(float value)
         {
-            Debug.Log("AWAKE");
+            _interval = value;
             
+            if (!Application.isPlaying)
+                return;
+                
+            UpdateSpawns();
+        }
+
+        void Start() {
+            _interval = 1;
             if (pathCreator != null)
             {
                 // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
                 pathCreator.pathUpdated += OnPathChanged;
                 transform.SetParent(pathCreator.gameObject.transform);
                 transform.position = new Vector3(0,0,0);
+                UpdateSpawns();
             }
         }
+
 
 
         // If the path changes during the game, update the distance travelled so that the follower's position on the new path
         // is as close as possible to its position on the old path
         void OnPathChanged() {
+            UpdateSpawns();
+        }
+
+        private void OnIntervalChanged()
+        {
+            UpdateSpawns();
+        }
+
+        public void UpdateSpawns()
+        {
             ClearChildren();
             SpawnAtInterval();
         }
@@ -48,15 +90,15 @@ namespace PathCreation
 
         void SpawnAtInterval()
         {
-            totalDist = pathCreator.path.length;
-            int numSpawn = (int)(totalDist/interval);
+            _totalDist = pathCreator.path.length;
+            int numSpawn = (int)(_totalDist/_interval);
             for(int i = 0; i <= numSpawn; i++)
             {
-                GameObject instance = Instantiate(toSpawnPrefab);
-                instance.transform.SetParent(transform);
-                instance.transform.position = pathCreator.path.GetPointAtDistance(interval*i, EndOfPathInstruction.Stop);
+                GameObject instance = Instantiate(toSpawnPrefab, transform);
+                instance.transform.position = pathCreator.path.GetPointAtDistance(_interval*i, EndOfPathInstruction.Stop);
             }
+            
         }
-    }
 
+    }
 }
