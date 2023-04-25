5615d4942372d519172772cbd0ef5227655de514
sebastianMF <smolanof@gmail.com>
2023-04-25 14:22:46 -0500

    Added WFC connector and model base

--- Assets/Scenes/Tests/Wave function collapse/WFC 2D Horizontal/SO Definitions/WFC2DConnector.cs
+++ Assets/Scenes/Tests/Wave function collapse/WFC 2D Horizontal/SO Definitions/WFC2DConnector.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 
 
-// This class is meant to be used to connect multiple WFC grids together. 
+// This class is meant to be used to connect multiple WFC Models together. 
 public class WFC2DConnector : MonoBehaviour
 {
-    
+    public int with = 1;
+    public int height = 1;
 }
